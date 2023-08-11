3990f8c0fadcfd29fe3b86b3a2cd8f28d91acd09
muhammadali0123 <136846620+muhammadali0123@users.noreply.github.com>
2023-08-12 00:11:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,15 +13,15 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
   var dc = {};
 
-  var homeHtmlUrl = "snippets/home-snippet.html";
+  var homeHtmlUrl = "home-snippet.html";
   var allCategoriesUrl =
     "https://davids-restaurant.herokuapp.com/categories.json";
-  var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-  var categoryHtml = "snippets/category-snippet.html";
+  var categoriesTitleHtml = "categories-title-snippet.html";
+  var categoryHtml = "category-snippet.html";
   var menuItemsUrl =
     "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
-  var menuItemsTitleHtml = "snippets/menu-items-title.html";
-  var menuItemHtml = "snippets/menu-item.html";
+  var menuItemsTitleHtml = "menu-items-title.html";
+  var menuItemHtml = "menu-item.html";
 
   // Convenience function for inserting innerHTML for 'select'
   var insertHtml = function (selector, html) {
@@ -32,7 +32,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
   // Show loading icon inside element identified by 'selector'.
   var showLoading = function (selector) {
     var html = "<div class='text-center'>";
-    html += "<img src='images/ajax-loader.gif'></div>";
+    html += "<img src='ajax-loader.gif'></div>";
     insertHtml(selector, html);
   };
 
